0d365c6bb2d5a0ee2053db0ab842158f473d1576
Douglas Albuquerque <douglasalbuquerque95@gmail.com>
2019-09-05 21:56:11 -0300

    Projeto criado durante o curso de React da rocketseat.com.br

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Routes from './routes';
+
+import "./styles.css";
+
+
+
+import Header from "./componentes/Header";
+import Main from "./pages/main";
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <Routes />
+  </div>
+);
+
+export default App;
---
+++ src/componentes/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import "./styles.css";
+
+//stateless component
+const Header = () => ( 
+    <header id="main-header">Training React</header>
+);
+
+export default Header;
